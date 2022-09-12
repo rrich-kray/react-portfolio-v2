ee5fe350464a8df7876c2e997e3294ca9897160b
Ryan Kray <rrich.kray@gmail.com>
2022-09-12 15:53:08 -0400

    stopping point

--- src/models/Room.js
+++ src/models/Room.js
@@ -3,16 +3,24 @@ import { useGLTF, Sparkles } from "@react-three/drei";
 
 export const Room = ({ props }) => {
   return (
-    <group {...props} dispose={null} position={[0, 0, 0]}>
+    <group {...props} dispose={null} position={[0, 0, 0]} rotate={[0, 0, 1]}>
       <mesh
         castShadow
         receiveShadow
-        position={[1, 2, 3]}
-        rotation={[1.6, 0.1, 3]}
+        position={[0, 1.875, 2.13]}
+        rotation={[1.565, 0, 3.15]}
       >
         <boxGeometry args={[4, 0.25, 4]} />
       </mesh>
       <mesh castShadow receiveShadow rotation={[0, 0, 0]}>
+        <boxGeometry args={[4, 0.25, 4]} />
+      </mesh>
+      <mesh
+        castShadow
+        receiveShadow
+        rotation={[1.58, 0, 4.7]}
+        position={[1.9, 1.86, 0]}
+      >
         <boxGeometry args={[4, 0.25, 4]} />
       </mesh>
     </group>
