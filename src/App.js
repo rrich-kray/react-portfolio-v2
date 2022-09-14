c47b5e7fca2626b97fcbae1d776ac83e845cf7e8
Ryan Kray <rrich.kray@gmail.com>
2022-09-14 11:24:06 -0400

    experimenting

--- src/App.js
+++ src/App.js
@@ -27,24 +27,26 @@ function App() {
       {isLoading ? (
         <Splash />
       ) : (
-        <div id="app">
-          {activePost.length !== 0 ? (
-            <ProjectPage
-              projectData={activePost}
-              setActivePost={setActivePost}
-            />
-          ) : (
-            <>
-              {/* <Nav /> */}
-              {/* <Hero /> */}
-              <Projects setActivePost={setActivePost} />
-              <AboutRedux />
-              {/* <About /> */}
-              {/* <Skills /> */}
-              {/* <Contact /> */}
-            </>
-          )}
-        </div>
+        <wrapper>
+          <div id="app">
+            {activePost.length !== 0 ? (
+              <ProjectPage
+                projectData={activePost}
+                setActivePost={setActivePost}
+              />
+            ) : (
+              <>
+                {/* <Nav /> */}
+                {/* <Hero /> */}
+                <Projects setActivePost={setActivePost} />
+                <AboutRedux />
+                {/* <About /> */}
+                {/* <Skills /> */}
+                {/* <Contact /> */}
+              </>
+            )}
+          </div>
+        </wrapper>
       )}
     </>
   );
